5d30289624b05a180ca060bc31d5b03e11b5ee0f
Anand-Ramnarain-27 <142887960+Anand-Ramnarain-27@users.noreply.github.com>
2026-08-20 17:49:46 +0200

    Replace splatWalkerUnity PNG with GIF
    Switch splatWalkerUnity asset from a static PNG to an animated GIF. Added src/assets/splatWalkerUnity.gif, removed src/assets/splatWalkerUnity.png, and updated the import in src/assets/index.js to reference the .gif (import splatWalkerUnity from "./splatWalkerUnity.gif"). This enables the animated asset to be used in the app.

--- src/assets/index.js
+++ src/assets/index.js
@@ -33,7 +33,7 @@ import broforce from "./broforce.png";
 import animation from "./animation.png";
 import Platformer from "./Platformer.png";
 import splatWalker from "./splatWalker.png";
-import splatWalkerUnity from "./splatWalkerUnity.png";
+import splatWalkerUnity from "./splatWalkerUnity.gif";
 import hacker from "./hack.png";
 import Tower from "./Tower.png";
 import team from "./team.png";
